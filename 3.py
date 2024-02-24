bb5ca6fcdea94ea86aa147f6f65d22e476cbc696
AUEUWORK <145907201+AUEUWORK@users.noreply.github.com>
2024-02-24 04:23:29 -0800

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+coruser = "admin"
+corpass = "Ad13n@23t"
+
+user = input("Username: ")
+passw = input("Password: ")
+
+if user == coruser and passw == corpass:
+    print("เข้าสู่ระบบสำเร็จ")
+else:
+    print("เข้าสู่ระบบไม่สำเร็จ")
